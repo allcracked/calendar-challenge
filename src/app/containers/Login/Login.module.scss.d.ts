879747a89ff37aa9b0222b8b2370e1b6f52df572
Jose Avilez <jose.avilez@partnerhero.com>
2020-02-25 17:07:20 -0600

    Added Styles for Login
    Included styles for the login screen.

--- src/app/containers/Login/Login.module.scss.d.ts
+++ src/app/containers/Login/Login.module.scss.d.ts
@@ -1,12 +1,12 @@
 declare namespace LoginModuleScssModule {
-  export interface ILoginModuleScss {
-    para: string;
-  }
+    export interface ILoginModuleScss {
+        mainContainer: string;
+    }
 }
 
 declare const LoginModuleScssModule: LoginModuleScssModule.ILoginModuleScss & {
-  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
-  locals: LoginModuleScssModule.ILoginModuleScss;
+    /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+    locals: LoginModuleScssModule.ILoginModuleScss;
 };
 
 export = LoginModuleScssModule;
--- src/app/containers/Login/Login.tsx
+++ src/app/containers/Login/Login.tsx
@@ -1,30 +1,52 @@
 import React from 'react';
 import firebase from 'firebase/app';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
 
 import { firebaseAuth } from '../../modules/Firebase/FirebaseApp';
 
 import styles from './Login.module.scss';
 
 const Login: React.FC = () => {
-    const login = (): void => {
+    const handleLogin = (): void => {
         const googleProvider = new firebase.auth.GoogleAuthProvider();
         firebaseAuth.signInWithPopup(googleProvider);
     };
 
-    const anonymousLogin = (): void => {
+    const handleAnonymousLogin = (): void => {
         firebaseAuth.signInAnonymously();
     };
 
     return (
-        <div>
-            <p className={styles.para}>This is a login page.</p>
-            <button type="button" onClick={login}>
-                Login
-            </button>
-            <button type="button" onClick={anonymousLogin}>
-                Anonymous Login
-            </button>
-        </div>
+        <>
+            <Container className={styles.mainContainer}>
+                <Row>
+                    <Col md={{ span: 6, offset: 3 }} className="text-center">
+                        <h1 className="text-center">Jobsity Calendar</h1>
+                        <br />
+                        <br />
+                        <Button variant="primary" size="lg" onClick={handleLogin}>
+                            Login with Google
+                        </Button>
+                        <br />
+                        <Button variant="link" onClick={handleAnonymousLogin}>
+                            Continue without Google
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+            <Navbar fixed="bottom">
+                <Container>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse>
+                        <Navbar.Text>Developed by Jose Avilez for Jobsity</Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
